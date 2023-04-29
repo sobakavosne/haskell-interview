770a8b9d24320597e45e7a640299186f5253a2c4
sobakavosne <sobakavosne@protonmail.com>
2023-04-29 11:44:05 +0400

    add tests

---
+++ tests/THExample.hs
@@ -0,0 +1,22 @@
+module Main where
+
+-- import           Test.HUnit
+
+-- -- The function we want to test
+-- double :: Int -> Int
+-- double x = x * 2
+
+-- -- Test cases
+-- testDouble :: Test
+-- testDouble = test
+--   [ "double 2" ~: double 2 ~?= 4
+--   , "double 0" ~: double 0 ~?= 0
+--   , "double -1" ~: double (-1) ~?= (-2)
+--   ]
+
+-- -- Run the tests
+-- main :: IO Counts
+-- main = runTestTT testDouble
+
+main :: IO ()
+main = print "Test suite"
