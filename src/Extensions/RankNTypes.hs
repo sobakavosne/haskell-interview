ada76c7475ad61044685a96fe9a737ea9270e9ef
lenchevskii <dima.lenchevskii@gmail.com>
2023-05-01 17:03:51 +0400

    remove GADTs

--- src/Extensions/RankNTypes.hs
+++ src/Extensions/RankNTypes.hs
@@ -12,19 +12,5 @@ rank p x =
   if p [x] x
     then x
     else 0
-
--- data Term a
---   = Lit Int
---   | Succ (Term Int)
---   | IsZero (Term Int)
---   | If (Term Bool) (Term a) (Term a)
---   | Pair (Term a) (Term b)           -- type b not in scope
---
-data Term a where
-  Lit :: Int -> Term Int
-  Succ :: Term Int -> Term Int
-  IsZero :: Term Int -> Term Bool
-  If :: Term Bool -> Term a -> Term a -> Term a
-  Pair :: Term a -> Term b -> Term (a, b)
 --
 -- $> rank (flip elem) 1
