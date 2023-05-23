79e6960f7a48ddf016ea68d14bbec9cbe4555c9c
lenchevskii <dima.lenchevskii@gmail.com>
2023-05-23 19:28:11 +0400

    add Nesting Depth challenge: python & haskell

---
+++ src/Challenges/NestingDepth.hs
@@ -0,0 +1,13 @@
+module Challenges.NestingDepth where
+
+-- code_report Solution
+-- Problem Link (Contest):  https://leetcode.com/contest/weekly-contest-210/problems/maximum-nesting-depth-of-the-parentheses/
+-- Problem Link (Practice): https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+
+maxDepth :: String -> Int
+maxDepth = maximum
+         . scanl1 (+)
+         . map (\ c -> if c == '(' then 1 else -1)
+         . filter (`elem` ['(',')'])
+
+-- $> maxDepth "(1 + (3 / (10 - 5)))"
---
+++ src/Challenges/NestingDepth.py
@@ -0,0 +1,35 @@
+import functools as F
+import builtins as B
+
+
+def compose(*fns):
+  return F.reduce(
+    lambda f, g: lambda x: f(g(x)), 
+    fns, 
+    lambda x: x
+  )
+
+
+def scan(lst, acc = 0):
+  return [acc := acc + x for x in lst]
+
+
+def mapBraces(lst):
+  return map(lambda x: 1 if x == '(' else 0, lst)
+
+
+def filterBraces(lst):
+  return filter(lambda x: x in ['(', ')'], lst)
+
+
+def maxDepth(string):
+  return compose(
+    B.max,
+    scan,
+    lambda lst: map(lambda x: 1 if x == '(' else 0, lst),
+    filterBraces,
+    list
+  )(string)
+
+
+print("maxDepth: ", maxDepth("(1 + (3 / (10 - 5)))"))
