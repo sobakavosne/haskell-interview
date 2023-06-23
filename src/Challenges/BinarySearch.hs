49680fb968373a5ff15ae31ca235f3ac9f424a54
lenchevskii <dima.lenchevskii@gmail.com>
2023-06-23 19:35:07 +0200

    add binary search implementation

---
+++ src/Challenges/BinarySearch.hs
@@ -0,0 +1,15 @@
+module Challenges.BinarySearch where
+
+import qualified GHC.Base as B
+
+binarySearch :: Ord a => [a] -> a -> Maybe a
+binarySearch l@(x:xs) item
+  | x > item || last xs < item = Nothing
+  | otherwise = (binarySplit . splitAt (B.divInt (length l) 2)) l
+  where
+    binarySplit (y:ys, z:zs)
+      | z > item = binarySearch (y : ys) item
+      | z < item = binarySearch zs item
+      | otherwise = Just item
+--
+-- $> binarySearch [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 8
