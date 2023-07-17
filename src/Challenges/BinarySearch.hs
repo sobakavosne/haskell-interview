983700ad7b73c5dc3141085b60df588a19dc3e95
lenchevskii <dima.lenchevskii@gmail.com>
2023-07-17 09:43:51 +0200

    fix: `empty list` exception

--- src/Challenges/BinarySearch.hs
+++ src/Challenges/BinarySearch.hs
@@ -4,14 +4,12 @@ import qualified GHC.Base as B
 
 binarySearch :: Ord a => [a] -> a -> Maybe a
 binarySearch l@(x:xs) item
-  | x > item || last xs < item = Nothing
+  | item `notElem` l = Nothing
   | otherwise = (binarySplit . splitAt (B.divInt (length l) 2)) l
   where
     binarySplit (y:ys, z:zs)
       | z > item = binarySearch (y : ys) item
       | z < item = binarySearch zs item
       | otherwise = Just item
---
--- TODO: fix `empty list` exception
 --
 -- $> binarySearch [1, 2, 3, 4, 5, 9, 10] 8
