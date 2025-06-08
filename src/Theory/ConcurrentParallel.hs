8f4e83d32d4ef6e1ec5ea91789cfa068f452a2c9
Λ <sobakavosne@protonmail.com>
2025-06-08 14:58:55 +0200

    fix(Theory): update `mainT` with `async`

--- src/Theory/ConcurrentParallel.hs
+++ src/Theory/ConcurrentParallel.hs
@@ -29,42 +29,53 @@
 
 module Theory.ConcurrentParallel where
 
-import           Control.Monad (replicateM)
-import           Data.IORef    (IORef, newIORef, readIORef)
-import           GHC.Conc      (STM, TVar, atomically, newTVar, readTVar,
-                                writeTVar)
-import           GHC.IORef     (writeIORef)
+import           Control.Concurrent.Async (async, wait)
+import           Control.Monad            (replicateM)
+import           Data.IORef               (IORef, newIORef, readIORef)
+import           GHC.Conc                 (TVar, atomically, newTVarIO,
+                                           readTVar, readTVarIO, threadDelay,
+                                           writeTVar)
+import           GHC.IORef                (writeIORef)
 
-incrementer :: Num a => IORef a -> IO [()]
-incrementer ref = do
+incrementor :: Num a => IORef a -> IO [()]
+incrementor ref = do
   replicateM
     10
-    do x <- readIORef ref
+    do
+      x <- readIORef ref
     -- Here is the switching of the process can appear
-    -- and the data race is occured
-       writeIORef ref (x + 1)
+    -- and the data race is occurred
+      writeIORef ref (x + 1)
 
 main :: IO ()
 main = do
-  counter <- newIORef 0
-  -- async (incrementer counter)
-  -- async (incrementer counter)
-  -- threadDelay 1000
+  counter <- newIORef (0 :: Integer)
+  _ <- async (incrementor counter)
+  _ <- async (incrementor counter)
+  threadDelay 10
   print =<< readIORef counter -- waiting for 20, but it can be not: data race
 
 -- To avoid data race use STM (Software Transactional Memory)
-incrementerT :: TVar Int -> IO [()]
-incrementerT ref = do
+incrementorT :: TVar Int -> IO [()]
+incrementorT ref = do
   replicateM
     10
-    do atomically
-         do x <- readTVar ref
-            writeTVar ref (x + 1)
+    do
+      atomically
+        do
+          x <- readTVar ref
+          writeTVar ref (x + 1)
 
-mainT :: STM Integer
+mainT :: IO ()
 mainT = do
-  counter <- newTVar 0
-  -- async (incrementer counter)
-  -- async (incrementer counter)
-  -- threadDelay 1000
-  readTVar counter -- будет точно 20
+  counter <- newTVarIO 0 -- newTVarIO ≡ atomically . newTVar
+  a1 <- async (incrementorT counter)
+  a2 <- async (incrementorT counter)
+  _ <- wait a1
+  _ <- wait a2
+  final <- readTVarIO counter -- readTVarIO ≡ atomically . readTVar
+  print final -- guaranteed to be 20
+--
+-- $> main
+--
+-- $> mainT
